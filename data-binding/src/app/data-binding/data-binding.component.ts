b16276f004b8c1eede30aef52ebb19634a9cd9d2
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-18 12:01:26 -0300

    FEAT: Aula 09
    Property Binding e Interpolation

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss']
+  styleUrls: ['./data-binding.component.scss'],
 })
 export class DataBindingComponent implements OnInit {
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'http://lorempixel.com.br/400/200/nature';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getValor() {
+    return 1;
   }
 
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
