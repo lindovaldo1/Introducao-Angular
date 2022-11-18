7eef572be403b349148885168774be18d2f19cae
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-18 09:55:25 -0300

    FEAT: Aula 05
    Modules (ngModule)

--- meu-primeiro/src/app/app.module.ts
+++ meu-primeiro/src/app/app.module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MeuPrimeiroComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MeuPrimeiroComponent],
+  imports: [BrowserModule, AppRoutingModule, CursosModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ meu-primeiro/src/app/cursos/cursos.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+
+@NgModule({
+  declarations: [CursosComponent, CursoDetalheComponent],
+  imports: [CommonModule],
+  exports: [CursosComponent],
+})
+export class CursosModule {}
