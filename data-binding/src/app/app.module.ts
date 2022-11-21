ecc84206a09b81e45387c78a31f42cc63ed90f03
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-21 15:11:19 -0300

    FEAT: Aula 12
    Two-way data binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
@@ -8,7 +10,13 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [AppComponent, DataBindingComponent],
-  imports: [BrowserModule, AppRoutingModule, TooltipModule.forRoot()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TooltipModule.forRoot(),
+    FormsModule,
+    MeuFormModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -6,41 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-binding.component.scss'],
 })
 export class DataBindingComponent implements OnInit {
-  url: string = 'http://loiane.com';
-  cursoAngular: boolean = true;
-  urlImagem: string = 'http://lorempixel.com.br/400/200/nature';
-
-  valorAtual: string = '';
-  valorSalvo: string = '';
-
-  isMouseOver: boolean = false;
-
-  getValor() {
-    return 1;
-  }
-
-  getCurtirCurso() {
-    return true;
-  }
-
-  botaoClicado() {
-    alert('Botão clicado!');
-  }
-
-  onKeyUp(evento: KeyboardEvent) {
-    this.valorAtual = (<HTMLInputElement>evento.target).value;
-  }
-
-  salvarValor(valor: string) {
-    this.valorSalvo = valor;
-  }
-
-  onMouseOver() {
-    this.isMouseOver = !this.isMouseOver;
-  }
-
-  onMouseOverOut() {}
-
   constructor() {}
 
   ngOnInit(): void {}
